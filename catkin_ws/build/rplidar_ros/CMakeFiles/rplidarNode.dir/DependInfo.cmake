5403cc0aff5c60f96720d2bf16638d1329f4d0bb
Ben <benjaminpablotran@gmail.com>
2026-01-15 22:11:39 +0000

    Add catkin workspace

---
+++ catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/arch/linux/net_serial.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/arch/linux/net_serial.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/arch/linux/net_socket.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/arch/linux/net_socket.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/arch/linux/timer.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/arch/linux/timer.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/dataunpacker/dataunpacker.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/dataunpacker/dataunpacker.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/dataunpacker/unpacker/handler_capsules.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/dataunpacker/unpacker/handler_capsules.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/dataunpacker/unpacker/handler_hqnode.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/dataunpacker/unpacker/handler_hqnode.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/dataunpacker/unpacker/handler_normalnode.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/dataunpacker/unpacker/handler_normalnode.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/hal/thread.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/hal/thread.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/rplidar_driver.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/rplidar_driver.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/sl_async_transceiver.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/sl_async_transceiver.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/sl_crc.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/sl_crc.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/sl_lidar_driver.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/sl_lidar_driver.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/sl_lidarprotocol_codec.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/sl_lidarprotocol_codec.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/sl_serial_channel.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/sl_serial_channel.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/sl_tcp_channel.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/sl_tcp_channel.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/sdk/src/sl_udp_channel.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/sdk/src/sl_udp_channel.cpp.o"
+  "/root/catkin_ws/src/rplidar_ros/src/node.cpp" "/root/catkin_ws/build/rplidar_ros/CMakeFiles/rplidarNode.dir/src/node.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"rplidar_ros\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/root/catkin_ws/src/rplidar_ros/./sdk/include"
+  "/root/catkin_ws/src/rplidar_ros/./sdk/src"
+  "/opt/ros/noetic/include"
+  "/opt/ros/noetic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
