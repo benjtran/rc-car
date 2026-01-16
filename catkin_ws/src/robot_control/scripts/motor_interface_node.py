8159dd3b09f4242d20161108ebdcfc3c8bfbf11b
Ben <benjaminpablotran@gmail.com>
2026-01-16 05:14:24 +0000

    Add send velocity commands from motor interface node to arduino

--- catkin_ws/src/robot_control/scripts/motor_interface_node.py
+++ catkin_ws/src/robot_control/scripts/motor_interface_node.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python3
+
+"""
+Motor Interface node
+
+- Recieves desired velocity from path planner node
+- Sends velocity command via serial to motors
+"""
+
 import rospy
+import serial
+import time
+
+PORT = '/dev/ttyACM0'
+BAUD_RATE = 9600
+
+def send_command(ser, cmd):
+    ser.write((cmd + '\n').encode())
 
 def main():
     rospy.init_node('motor_interface', anonymous=True)
     rospy.loginfo("Motor interface node started!")
-    rospy.spin()
+    ser = serial.Serial(PORT, BAUD_RATE, timeout=1)
+    time.sleep(2)
+    rate = rospy.Rate(0.5)
+    while not rospy.is_shutdown():
+        send_command(ser, "24.4")
+        time.sleep(2)
+        send_command(ser, "0.0")
+        time.sleep(2)
+    ser.close()
 
 if __name__ == "__main__":
     main()
+
